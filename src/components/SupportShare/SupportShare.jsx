e05051bcfcd0c536c1feb0ec729214d29421e4e9
lksndr1 <150901178+lksndr1@users.noreply.github.com>
2024-07-16 16:55:03 +0200

    Revert "Feature/share+footer clone"

--- src/components/SupportShare/SupportShare.jsx
+++ src/components/SupportShare/SupportShare.jsx
@@ -1,35 +1,5 @@
-import { useState } from 'react';
-import share from '../../assets/buttonIcons/share.svg';
 import s from './styles.module.scss';
-import ModalShare from '../ModalShare/ModalShare';
-import Button from '../ui/Button/Button.jsx';
 
 export const SupportShare = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  return (
-    <section className={s.SupportShare}>
-      <h2>
-        допоможи <br />
-        <span>іншим дізнатись</span> <br />
-        про наш збір
-      </h2>
-      <Button
-        className={s.share_btn}
-        text="Поділитись"
-        hoverText="Поділитись"
-        activeText="Поділитись"
-        icon={<img src={share} alt="Share" />}
-        onClick={openModal}
-        type="share"
-      />
-
-      {isModalOpen && <ModalShare onClose={closeModal} />}
-    </section>
-  );
+  return <section className={s.SupportShare}>SupportShare</section>;
 };
--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,9 +1,5 @@
 import s from './styles.module.scss';
 
 export const Footer = () => {
-  return (
-    <footer className={`${s.footer} ${s.container}`}>
-      <p>Розробка Baza Trainee Ukraine 2024 © Всі права захищені</p>
-    </footer>
-  );
+  return <footer className={s.footer}>Footer</footer>;
 };
