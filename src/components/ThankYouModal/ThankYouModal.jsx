b82a0a33ff2f1ee7f5308488d29be147c60355d6
Klima <klima3775@gmail.com>
2024-07-18 02:33:23 +0300

    completed

--- src/components/ThankYouModal/ThankYouModal.jsx
+++ src/components/ThankYouModal/ThankYouModal.jsx
@@ -1,17 +1,60 @@
+// // import s from './styles.module.scss';
+// // import Button from '../ui/Button/Button';
+// // import heart from '../../assets/modal/svg/Heart.svg';
+// // // import airplane from '../../assets/buttonIcons/plane.svg';
+
+// // export const ThankYouModal = ({ onClose }) => {
+// //   const handleClick = () => {
+// //     window.location.href = 'https://www.example.com'; // замінити на потрібне посилання
+// //   };
+// //   return (
+// //     <div className={s.overlay}>
+// //       <div className={s.modal}>
+// //         <button className={s.closeButton} onClick={onClose}>
+// //           &times;
+// //         </button>
+// //         <div className={s.header}>ДЯКУЄМО ЗА ТВІЙ ДОНАТ!</div>
+// //         <div className={s.icon}>
+// //           <img src={heart} alt="heart" />
+// //         </div>
+// //         <p className={s.description}>
+// //           Кожен твій донат допомагає нищити ворогів і наближати нашу Перемогу!
+// //         </p>
+// //         <h2 className={s.subtitle}>Слава Україні! Героям Слава</h2>
+// //         <div className={s.buttonContainer}>
+// //           <Button
+// //             text="НА ГОЛОВНУ"
+// //             hoverText="ЛЕТИТЬ МІЙ ДОНАТ"
+// //             icon={<img src={heart} alt="heart" />}
+// //             activeText="ЛЕТИТЬ МІЙ ДОНАТ"
+// //             type="join"
+// //             className={s.join_btn}
+// //             onClick={handleClick}
+// //           />
+// //         </div>
+// //       </div>
+// //     </div>
+// //   );
+// // };
+
+// // export default ThankYouModal;
+
+// import PropTypes from 'prop-types';
 // import s from './styles.module.scss';
 // import Button from '../ui/Button/Button';
 // import heart from '../../assets/modal/svg/Heart.svg';
-// // import airplane from '../../assets/buttonIcons/plane.svg';
-
-// export const ThankYouModal = ({ onClose }) => {
+// import plane from '../../assets/buttonIcons/plane.svg';
+// import x from '../../assets/modal/svg/X.svg';
+// const ThankYouModal = ({ onClose }) => {
 //   const handleClick = () => {
 //     window.location.href = 'https://www.example.com'; // замінити на потрібне посилання
 //   };
+
 //   return (
 //     <div className={s.overlay}>
 //       <div className={s.modal}>
 //         <button className={s.closeButton} onClick={onClose}>
-//           &times;
+//           <img src={x} alt="x" />
 //         </button>
 //         <div className={s.header}>ДЯКУЄМО ЗА ТВІЙ ДОНАТ!</div>
 //         <div className={s.icon}>
@@ -25,16 +68,20 @@
 //           <Button
 //             text="НА ГОЛОВНУ"
 //             hoverText="ЛЕТИТЬ МІЙ ДОНАТ"
-//             icon={<img src={heart} alt="heart" />}
+//             icon={<img src={plane} alt="heart" />}
 //             activeText="ЛЕТИТЬ МІЙ ДОНАТ"
-//             type="join"
-//             className={s.join_btn}
+//             type="toMain"
+//             className={s.main_btn}
 //             onClick={handleClick}
 //           />
 //         </div>
 //       </div>
 //     </div>
 //   );
+// };
+
+// ThankYouModal.propTypes = {
+//   onClose: PropTypes.func.isRequired
 // };
 
 // export default ThankYouModal;
@@ -44,6 +91,8 @@ import s from './styles.module.scss';
 import Button from '../ui/Button/Button';
 import heart from '../../assets/modal/svg/Heart.svg';
 import plane from '../../assets/buttonIcons/plane.svg';
+import x from '../../assets/modal/svg/X.svg';
+
 const ThankYouModal = ({ onClose }) => {
   const handleClick = () => {
     window.location.href = 'https://www.example.com'; // замінити на потрібне посилання
@@ -53,9 +102,11 @@ const ThankYouModal = ({ onClose }) => {
     <div className={s.overlay}>
       <div className={s.modal}>
         <button className={s.closeButton} onClick={onClose}>
-          &times;
+          <img src={x} alt="x" />
         </button>
-        <div className={s.header}>ДЯКУЄМО ЗА ТВІЙ ДОНАТ!</div>
+        <div className={s.header}>
+          ДЯКУЄМО ЗА <span className={s.highlight}>ТВІЙ ДОНАТ!</span>
+        </div>
         <div className={s.icon}>
           <img src={heart} alt="heart" />
         </div>
@@ -67,7 +118,7 @@ const ThankYouModal = ({ onClose }) => {
           <Button
             text="НА ГОЛОВНУ"
             hoverText="ЛЕТИТЬ МІЙ ДОНАТ"
-            icon={<img src={plane} alt="heart" />}
+            icon={<img src={plane} alt="plane" />}
             activeText="ЛЕТИТЬ МІЙ ДОНАТ"
             type="toMain"
             className={s.main_btn}
