39ed567588b178e765529327ae5532301b1e56bb
Oksana Baranik <ksyubaranik@gmail.com>
2024-07-24 12:15:12 +0200

    Added image for mobile version in How it works section

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -1,5 +1,14 @@
 import s from './styles.module.scss';
+import imgMobile from '../../assets/imagesHowItWorks/img-mobile.png';
 
 export const HowItWorks = () => {
-  return <section className={s.HowItWorks}>HowItWorks</section>;
+  return (
+    <section className={s.HowItWorks}>
+      <div className={s.HowItWorks__container}>
+        <div className={s.HowItWorks__img}>
+          <img src={imgMobile} alt="How it works" />
+        </div>
+      </div>
+    </section>
+  );
 };
