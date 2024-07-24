590d6e2eec0efbf5fdfe2d7f66a5b2b37fc2c064
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-07-24 15:57:00 +0300

    fix problems after merge

--- src/components/WhyItMatters/WhyItMatters.jsx
+++ src/components/WhyItMatters/WhyItMatters.jsx
@@ -1,215 +1,3 @@
-// import { useState } from 'react';
-// import s from './styles.module.scss';
-// import glasses from '../../assets/imageswhyitmatters/glasses.png';
-// import antenna from '../../assets/imageswhyitmatters/antenna.png';
-// import happymodel from '../../assets/imageswhyitmatters/happymodel.png';
-// import radiomaster from '../../assets/imageswhyitmatters/radiomaster.png';
-// import propellers from '../../assets/imageswhyitmatters/propellers.png';
-// import Button from '../ui/Button/Button';
-// import plane from '../../assets/buttonIcons/plane.svg';
-// import ThankYouModal from '../ThankYouModal/ThankYouModal';
-
-// export const WhyItMatters = () => {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   const handleButtonClick = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const handleCloseModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   return (
-//     <section className={s.WhyItMatters}>
-//       <div className={s.WhyItMatters__container}>
-//         <div className={s.WhyItMatters__wrapper}>
-//           <div className={s.WhyItMatters__upper}>
-//             <h1>
-//               Чому це
-//               <br />
-//               <span>важливо?</span>
-//             </h1>
-//             <p>
-//               Докупівля обладнання із переліку дозволить бригаді проводити
-//               розвідку, коригувати вогонь, наводити на цілі, рятувати поранених.
-//             </p>
-//           </div>
-//           <div className={s.WhyItMatters__glasses}>
-//             <div className={s.WhyItMatters__img}>
-//               <img src={glasses} alt="Glasses" />
-// =======
-//         {/* Mobile and Tablet screen */}
-//         <div className={s.WhyItMatters__small_wrapper}>
-//           <div className={s.WhyItMatters__nodot_container}>
-//             <div className={s.WhyItMatters__upper}>
-//               <h2 className={s.upper_title}>
-//                 Чому це
-//                 <br />
-//                 <span>важливо?</span>
-//               </h2>
-//               <p>
-//                 Докупівля обладнання із переліку дозволить бригаді проводити
-//                 розвідку, коригувати вогонь, наводити на цілі, рятувати
-//                 поранених.
-//               </p>
-//             </div>
-//             <div className={s.WhyItMatters__glasses}>
-//               <div className={s.WhyItMatters__img}>
-//                 <img src={glasses} alt="Glasses" />
-//               </div>
-//               <p className={`${s.WhyItMatters__text} ${s.glasses__text}`}>
-//                 Окуляри FPV Skyzone SKY04X PRO
-//               </p>
-//             </div>
-//           </div>
-
-//           <div
-//             className={`${s.WhyItMatters__dot_container} ${s.WhyItMatters__dot_container_up}`}
-//           >
-//             <div className={s.WhyItMatters__antenna}>
-//               <div className={s.WhyItMatters__img}>
-//                 <img src={antenna} alt="Antenna" />
-//               </div>
-//               <p className={`${s.WhyItMatters__text} ${s.antenna__text}`}>
-//                 FPV-антена Maple Wireless 5.8G
-//               </p>
-//             </div>
-//             <div className={s.WhyItMatters__happymodel}>
-//               <div className={s.WhyItMatters__img}>
-//                 <img src={happymodel} alt="Happymodel" />
-//               </div>
-//               <p className={`${s.WhyItMatters__text} ${s.happymodel__text}`}>
-//                 Модуль Happymodel ELRS ES900TX
-//               </p>
-//             </div>
-//           </div>
-
-//           <div
-//             className={`${s.WhyItMatters__dot_container} ${s.WhyItMatters__dot_container_down}`}
-//           >
-//             <div className={s.WhyItMatters__radiomaster}>
-//               <div className={s.WhyItMatters__img}>
-//                 <img src={radiomaster} alt="Radiomaster" />
-//               </div>
-//               <p className={`${s.WhyItMatters__text} ${s.radiomaster__text}`}>
-//                 Пульт RadioMaster Boxer
-//               </p>
-//             </div>
-//             <div className={s.WhyItMatters__propellers}>
-//               <div className={s.WhyItMatters__img}>
-//                 <img src={propellers} alt="Propellers" />
-//               </div>
-//               <p className={`${s.WhyItMatters__text} ${s.propellers__text}`}>
-//                 Пропелери HQP для 7 і 8-дюймових дронів
-//               </p>
-//             </div>
-//           </div>
-
-//           <div className={s.WhyItMatters__lower}>
-//             <p>
-//               Кожна гривня рятує найцінніше -<br /> життя військових.
-//             </p>
-//             <Button
-//               className={s.join_btn}
-//               text="Хочу доєднатись"
-//               hoverText="Летить мій донат"
-//               activeText="Летить мій донат"
-//               icon={<img src={plane} alt="Plane" />}
-//               onClick={handleButtonClick}
-//               type="join"
-//             />
-//           </div>
-//         </div>
-
-//         {/* Desktop screen */}
-//         <div className={s.WhyItMatters__big_wrapper}>
-//           <div className={s.WhyItMatters__upper}>
-//             <h2 className={s.upper_title}>
-//               Чому це
-//               <br />
-//               <span>важливо?</span>
-//             </h2>
-//             <p>
-//               Докупівля обладнання із переліку дозволить бригаді проводити
-//               розвідку, коригувати вогонь, наводити на цілі, рятувати поранених.
-//             </p>
-//           </div>
-
-//           <div className={s.WhyItMatters__big_container_upper}>
-//             <div className={s.WhyItMatters__glasses}>
-//               <div className={`${s.WhyItMatters__img} ${s.glasses__img}`}>
-//                 <img src={glasses} alt="Glasses" />
-//               </div>
-//               <p className={s.WhyItMatters__text}>
-//                 Окуляри FPV Skyzone SKY04X PRO
-//               </p>
-//             </div>
-
-//             <div className={s.WhyItMatters__antenna}>
-//               <div className={s.WhyItMatters__img}>
-//                 <img src={antenna} alt="Antenna" />
-//               </div>
-//               <p className={s.WhyItMatters__text}>
-//                 FPV-антена Maple Wireless 5.8G
-//               </p>
-//             </div>
-
-//             <div className={s.WhyItMatters__happymodel}>
-//               <div className={`${s.WhyItMatters__img} ${s.happymodel__img}`}>
-//                 <img src={happymodel} alt="Happymodel" />
-//               </div>
-//               <p className={`${s.WhyItMatters__text} ${s.happymodel__text}`}>
-//                 Модуль Happymodel ELRS ES900TX
-//               </p>
-//             </div>
-//           </div>
-
-//           <div className={s.WhyItMatters__big_container_lower}>
-//             <div className={s.lower_dot_container}>
-//               <div className={s.WhyItMatters__radiomaster}>
-//                 <div className={s.WhyItMatters__img}>
-//                   <img src={radiomaster} alt="Radiomaster" />
-//                 </div>
-//                 <p className={`${s.WhyItMatters__text} ${s.radiomaster__text}`}>
-//                   Пульт RadioMaster Boxer
-//                 </p>
-//               </div>
-//               <div className={s.WhyItMatters__propellers}>
-//                 <div className={`${s.WhyItMatters__img} ${s.propellers__img}`}>
-//                   <img src={propellers} alt="Propellers" />
-//                 </div>
-//                 <p className={`${s.WhyItMatters__text} ${s.propellers__text}`}>
-//                   Пропелери HQP для 7 і 8-дюймових дронів
-//                 </p>
-//               </div>
-//             </div>
-
-//             <div className={s.WhyItMatters__lower}>
-//               <p>
-//                 Кожна гривня рятує найцінніше -<br /> життя військових.
-//               </p>
-//               <Button
-//                 className={s.join_btn}
-//                 text="Хочу доєднатись"
-//                 hoverText="Летить мій донат"
-//                 activeText="Летить мій донат"
-//                 icon={<img src={plane} alt="Plane" />}
-//                 onClick={handleClick}
-//                 type="join"
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <section>
-//       {isModalOpen && <ThankYouModal onClose={handleCloseModal} />}
-//     </section>
-//   );
-// };
-//  КОД ЯКИЙ КРАШИТСЯ
-
-// Виправлений код
 import { useState } from 'react';
 import s from './styles.module.scss';
 import glasses from '../../assets/imageswhyitmatters/glasses.png';
@@ -251,18 +39,22 @@ export const WhyItMatters = () => {
             <div className={s.WhyItMatters__img}>
               <img src={glasses} alt="Glasses" />
             </div>
-            <p className={`${s.WhyItMatters__text} ${s.glasses__text}`}>
+            <p className={s.WhyItMatters__text}>
               Окуляри FPV Skyzone SKY04X PRO
             </p>
           </div>
+          <div
+            className={`${s.WhyItMatters__dot} ${s.WhyItMatters__firstdot}`}
+          ></div>
           <div className={s.WhyItMatters__antenna}>
             <div className={s.WhyItMatters__img}>
               <img src={antenna} alt="Antenna" />
             </div>
-            <p className={`${s.WhyItMatters__text} ${s.antenna__text}`}>
+            <p className={s.WhyItMatters__text}>
               FPV-антена Maple Wireless 5.8G
             </p>
           </div>
+          <div className={s.WhyItMatters__dot}></div>
           <div className={s.WhyItMatters__happymodel}>
             <div className={s.WhyItMatters__img}>
               <img src={happymodel} alt="Happymodel" />
@@ -279,11 +71,12 @@ export const WhyItMatters = () => {
               Пульт RadioMaster Boxer
             </p>
           </div>
+          <div className={s.WhyItMatters__dot}></div>
           <div className={s.WhyItMatters__propellers}>
             <div className={s.WhyItMatters__img}>
               <img src={propellers} alt="Propellers" />
             </div>
-            <p className={`${s.WhyItMatters__text} ${s.propellers__text}`}>
+            <p className={s.WhyItMatters__text}>
               Пропелери HQP для 7 і 8-дюймових дронів
             </p>
           </div>
@@ -293,9 +86,9 @@ export const WhyItMatters = () => {
             </p>
             <Button
               className={s.join_btn}
-              text="Хочу доєднатись"
-              hoverText="Летить мій донат"
-              activeText="Летить мій донат"
+              text="ХОЧУ ДОЄДНАТИСЬ"
+              hoverText="ЛЕТИТЬ МІЙ ДОНАТ"
+              activeText="ЛЕТИТЬ МІЙ ДОНАТ"
               icon={<img src={plane} alt="Plane" />}
               onClick={handleButtonClick}
               type="join"
